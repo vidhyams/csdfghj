9cfede9668c533653343a16f18e8788bc155ffda
vidhyams <30457499+vidhyams@users.noreply.github.com>
2017-07-28 05:42:03 -0400

    Rename sound.c to soundar.c

---
+++ soundar.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int m; 
+clrscr(); 
+printf("Enter Week day no = "); 
+scanf("%d",&m); 
+switch(a) 
+{ case 1: 
+printf("Sunday"); 
+break; 
+case 2: 
+printf("Monday"); 
+break; 
+case 3: 
+printf("Tuesday"); 
+break; 
+case 4: 
+printf("Wednesday"); 
+break; 
+case 5: 
+printf("Thrusday"); 
+break; 
+case 6: 
+printf("Friday"); 
+break; 
+case 7: 
+printf("Staturday"); 
+break; 
+default: 
+printf("Wrong Day NO Insert"); 
+} 
+getch(); 
+} 
+ 
